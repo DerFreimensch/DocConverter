99d5f4893a5917da7aefbf2ddf7e741165b92193
DerFreimensch <kostrm030102@gmail.com>
2021-04-08 17:45:10 +0300

    Добавьте файлы проекта.

---
+++ DocConverter/DocConverter.h
@@ -0,0 +1,32 @@
+﻿
+// DocConverter.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CDocConverterApp:
+// Сведения о реализации этого класса: DocConverter.cpp
+//
+
+class CDocConverterApp : public CWinApp
+{
+public:
+	CDocConverterApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CDocConverterApp theApp;
---
+++ DocConverter/DocConverterDlg.h
@@ -0,0 +1,34 @@
+﻿
+// DocConverterDlg.h: файл заголовка
+//
+
+#pragma once
+
+
+// Диалоговое окно CDocConverterDlg
+class CDocConverterDlg : public CDialogEx
+{
+// Создание
+public:
+	CDocConverterDlg(CWnd* pParent = nullptr);	// стандартный конструктор
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DOCCONVERTER_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// поддержка DDX/DDV
+
+
+// Реализация
+protected:
+	HICON m_hIcon;
+
+	// Созданные функции схемы сообщений
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
